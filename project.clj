84f1061e59de23a46770d9e607c657c8cb52e043
burinc <burin@duck.com>
2024-09-01 21:52:11 +1000

    Update project's dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mischov/reaver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.jsoup/jsoup "1.8.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [criterium "0.4.3"]]
+  :dependencies [[org.jsoup/jsoup "1.18.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0-rc2"]
+                                  [criterium "0.4.6"]]
                    :warn-on-reflection true}})
